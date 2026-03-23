fc359450c7e931957f03b2d110669b4ceeb6c70c
Emendir <dev@emendir.tech>
2026-03-23 18:45:04 +0100

    packaging(orchestration): new script `build_all.sh`

--- packaging/orchestration/build_all.sh
+++ packaging/orchestration/build_all.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-set -euo pipefail # Exit if any command fails
 
 # the absolute path of this script's directory
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-PROJ_DIR=$(realpath $SCRIPT_DIR/../..)
+PROJ_DIR=$(realpath "$SCRIPT_DIR/../..")
+BUILD_RUNNERS_DIR="$PROJ_DIR/packaging/build_runners"
 
-echo $PROJ_DIR
+ENDRA_BUILD_SKIP_APK=${ENDRA_BUILD_SKIP_APK-0}
+
+source "$PROJ_DIR/packaging/os_prereqs/os_platform_info.sh"
+
+set -euo pipefail # Exit if any command fails
+
+
 
 cd "$SCRIPT_DIR"
-#TODO
-echo "THIS SCRIPT IS NOT YET FUNCTIONAL"
+
+
+case "$OS" in
+    Linux)
+        "$BUILD_RUNNERS_DIR/flatpak/build_flatpak.sh"
+        "$BUILD_RUNNERS_DIR/pyinstaller/build_pyinstaller.sh"
+        if [ "$ENDRA_BUILD_SKIP_APK" -eq 0 ];then
+            "$BUILD_RUNNERS_DIR/buildozer/build_apk.sh"
+        fi
+        ;;
+    Darwin)
+        "$BUILD_RUNNERS_DIR/pyinstaller/build_pyinstaller.sh"
+        ;;
+    Windows)
+        "$BUILD_RUNNERS_DIR/pyinstaller/build_pyinstaller.sh"
+        ;;
+    *)
+        echo "Error: Unsupported platform: $OS"
+        exit 1
+        ;;
+esac
